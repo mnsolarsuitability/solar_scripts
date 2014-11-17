1c2a7d56ace3fc239dcd7099ed302ff2e7813e0e
Andy Walz <andywalz@gmail.com>
2014-11-17 09:02:26 -0600

    updated quick query script

--- run_a_quick_query.py
+++ run_a_quick_query.py
@@ -11,15 +11,15 @@ import dbconn
 #q = "UPDATE dem_fishnets SET state=0 WHERE state<>0 AND id IN (" + rr + ")"
 
 #q = "UPDATE dem_fishnets SET state=0 WHERE state=1 AND id <> 1164 AND id <> 1095 AND id <> 1094 AND id <> 1232"
-#q = """
-#    SELECT d.id FROM dem_fishnets d,sa_fishnets s WHERE ST_WITHIN(s.the_geom,d.the_geom) AND s.id=5578
-#"""
+q = """
+    SELECT d.id FROM dem_fishnets d,sa_fishnets s WHERE ST_WITHIN(s.the_geom,d.the_geom) AND s.id=5578
+"""
 #q = "UPDATE sa_fishnets SET state=0 FROM county c,sa_fishnets s WHERE c.countyname = 'Stearns' AND ST_INTERSECTS(c.the_geom,s.the_geom)"
 
 #q = "UPDATE sa_fishnets SET state=0 FROM county c,sa_fishnets s WHERE c.countyname = 'Stearns' AND ST_INTERSECTS(c.the_geom,s.the_geom)"
-q = "CREATE INDEX the_geom_ix ON sa_fishnets USING GIST (the_geom)"
+#q = "CREATE INDEX the_geom_ix ON sa_fishnets USING GIST (the_geom)"
 t = dbconn.run_query(q)
 
 
-#for s in t:
-#    print str(s['id'])
+for s in t:
+    print str(s['id'])
